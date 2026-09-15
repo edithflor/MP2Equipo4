3fbceb00154952f659bfe918ee8194368f32e1c5
Emilio Daniel Guzmán Seda <eguzmanseda@gmail.com>
2026-09-15 14:02:57 -0600

    refactor(F3-02): simplificar agrupacion de imagenes usando defaultdict

--- src/dataset_quality/imbalance.py
+++ src/dataset_quality/imbalance.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import Any
 
 
@@ -9,15 +10,14 @@ def analyze_class_imbalance(coco_data: dict[str, Any], min_images: int = 300) ->
     categories = {cat["id"]: cat["name"] for cat in coco_data.get("categories", [])}
     annotations = coco_data.get("annotations", [])
 
-    class_image_sets = {}
+    class_image_sets = defaultdict(set)
+
     for ann in annotations:
         cat_id = ann.get("category_id")
         img_id = ann.get("image_id")
 
         if cat_id is not None and img_id is not None:
             cat_name = categories.get(cat_id, "unknown")
-            if cat_name not in class_image_sets:
-                class_image_sets[cat_name] = set()
             class_image_sets[cat_name].add(img_id)
 
     class_counts = {name: len(img_set) for name, img_set in class_image_sets.items()}
