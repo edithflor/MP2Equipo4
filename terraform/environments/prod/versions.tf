a57c5b17bdde4b05ab07215187b8c45464cc7950
Emilio Daniel Guzmán Seda <eguzmanseda@gmail.com>
2026-09-18 22:03:37 -0600

    feat: despliegue de infra en S3 y versionado de dataset

--- terraform/environments/prod/versions.tf
+++ terraform/environments/prod/versions.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">= 1.9, < 2.0"
 
-  backend "s3" {
-    bucket       = "mp2-equipo4-terraform-state-prod"
-    key          = "prod/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = true
+backend "s3" {
+    bucket         = "mp2-equipo4-tfstate-emilio"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    use_lockfile   = true
+    encrypt        = true
   }
 
   required_providers {
